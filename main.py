b48faa6028fc24578aa8c20a18070db257f08ab6
Neomatri <lartigauthiago16@gmail.com>
2023-10-11 22:47:58 -0300

    TERMINADOOOOOOOOOOOOOOOOOOOOOOOOO

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 lista = [1, 2, 3, 4, 5, 6]
 condicionalSi = ["SI", "Si", "sI", "si"] 
-condicionalNum = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+condicionalNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 def agregarObjeto(): 
     agregar = input("¿Desea agregar o modificar un elemento a la lista? ")
-    if (agregar in condicionalSi): #comprueba si el valor ingresado esta en la lista "condicionalSi"
+    if (agregar in condicionalSi):
         agregado = input("¿Que desea agregar? ")
         agregadoPos = int(input("¿En que posicion? "))
-        if (agregado.isalpha): # Comprueba que se escriban letras
-            lista.insert(int(agregadoPos), agregado) #Inserta en la lista los nuevos datos [Posicion, dato]
-            print(f"Listo, ahora tu lista es {lista}")
-        elif(agregado in condicionalNum):
-            lista.insert(int(agregadoPos), int(agregado))
-            print(f"Listo, ahora tu lista es {lista}")
-        elif (agregadoPos >= 0 and agregadoPos <= len(lista) - 1 ): #"{len(lista) - 1}" marca el ultimo objeto en la lista
-            lista[int(agregadoPos)] = int(agregado) #Convierte los datos a numeros de la nueva lista
+        if (agregadoPos < 0 or agregadoPos > (len(lista) - 1)):
+            print("Error: Escriba la posicion dentro de los limites.")
+        elif (agregado.isalpha()): # Comprueba que se escriban letras
+            lista.insert(agregadoPos, agregado) # Inserta en la lista los nuevos datos [Posicion, dato]
             print(f"Listo, ahora tu lista es {lista}")
         else:
-            pass
+            lista.insert(agregadoPos, int(agregado)) # Convierte los datos a números de la nueva lista
+            print(f"Listo, ahora tu lista es {lista}")
     else:
         pass
 
-agregarObjeto() 
+agregarObjeto()
 
 def eliminarObjeto():
     eliminar = input("¿Desea eliminar un item? ")
     if (eliminar in condicionalSi):
         elimarObjPos = int(input(f"Elija la posicion del objeto a eliminar entre 0 y {len(lista) - 1} ")) #Comprueba si se encuentra en el rango de la lista "{len(lista) - 1}" marca el ultimo objeto en la lista
-        if(elimarObjPos < 0 or elimarObjPos > len(lista) - 1): #Comprueba si el valor introducido es distinto a los limites de la lista
+        if(elimarObjPos < 0 or elimarObjPos > (len(lista) - 1)): #Comprueba si el valor introducido es distinto a los limites de la lista
             print("Escriba un numero entre los parametros dichos ")
         else:
             lista.pop(elimarObjPos) #Elimina el objeto en la posicion seleccionada
